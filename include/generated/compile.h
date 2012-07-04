26a7d3accb4bdcf959e7ba286566e62a41f95289
sakuramilk <c.sakuramilk@gmail.com>
2012-07-04 19:34:04 +0900

    Initial commit SC-06D_ICS_Opensource

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Wed Jun 20 16:47:14 KST 2012"
+#define LINUX_COMPILE_BY "jiwooks"
+#define LINUX_COMPILE_HOST "jiwooks-desktop"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
