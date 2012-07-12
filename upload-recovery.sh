95d221611f1ade45f096fd1561b94e8159df7e51
sakuramilk <c.sakuramilk@gmail.com>
2012-07-13 01:16:25 +0900

    build: uoload recovery after reboot to recovery mode

--- upload-recovery.sh
+++ upload-recovery.sh
@@ -3,4 +3,4 @@
 adb push ./out/bin/recovery.img /data/local/tmp/recovery.img
 adb shell su -c "cat /data/local/tmp/recovery.img > /dev/block/platform/msm_sdcc.1/by-name/recovery"
 adb shell su -c "rm /data/local/tmp/recovery.img"
-adb shell su -c "sync;sync;sync;sleep 2; reboot"
+adb shell su -c "sync;sync;sync;sleep 2; reboot recovery"
