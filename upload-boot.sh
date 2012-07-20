c5d95e165f3ef17a0fca3fc111e2c9e0f0e00321
sakuramilk <c.sakuramilk@gmail.com>
2012-07-20 23:08:14 +0900

    build: change update script

--- upload-boot.sh
+++ upload-boot.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-adb push ./out/bin/boot.img /data/local/tmp/boot.img
-adb shell su -c "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
-adb shell su -c "rm /data/local/tmp/boot.img"
-adb shell su -c "sync;sync;sync;sleep 2; reboot"
+NOSU=$1
+
+if [ "$NOSU" = "nosu" ]; then
+#  adb push ./out/bin/boot.img /tmp/boot.img
+  adb push ./out/bin/boot.img /data/local/tmp/boot.img
+#  adb shell "cat /tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
+  adb shell "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
+  adb shell "rm /tmp/boot.img"
+  adb shell "sync;sync;sync;sleep 2; reboot"
+else
+  adb push ./out/bin/boot.img /data/local/tmp/boot.img
+  adb shell su -c "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
+  adb shell su -c "rm /data/local/tmp/boot.img"
+  adb shell su -c "sync;sync;sync;sleep 2; reboot"
+fi
--- upload-recovery.sh
+++ upload-recovery.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-adb push ./out/bin/recovery.img /data/local/tmp/recovery.img
-adb shell su -c "cat /data/local/tmp/recovery.img > /dev/block/platform/msm_sdcc.1/by-name/recovery"
-adb shell su -c "rm /data/local/tmp/recovery.img"
-adb shell su -c "sync;sync;sync;sleep 2; reboot recovery"
+NOSU=$1
+
+if [ "$NOSU" = "nosu" ]; then
+  adb push ./out/bin/recovery.img /tmp/recovery.img
+  adb shell "cat /tmp/recovery.img > /dev/block/platform/msm_sdcc.1/by-name/recovery"
+  adb shell "rm /tmp/recovery.img"
+  adb shell "sync;sync;sync;sleep 2; reboot recovery"
+else
+  adb push ./out/bin/recovery.img /data/local/tmp/recovery.img
+  adb shell su -c "cat /data/local/tmp/recovery.img > /dev/block/platform/msm_sdcc.1/by-name/recovery"
+  adb shell su -c "rm /data/local/tmp/recovery.img"
+  adb shell su -c "sync;sync;sync;sleep 2; reboot recovery"
+fi
