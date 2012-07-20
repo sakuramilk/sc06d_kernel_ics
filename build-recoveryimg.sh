e43935a45e2be37bb741393bf1a32f985da923a4
sakuramilk <c.sakuramilk@gmail.com>
2012-07-20 15:23:32 +0900

    build: Add --pagesize to mkbootimg args list

--- build-recoveryimg.sh
+++ build-recoveryimg.sh
@@ -34,7 +34,7 @@ echo "----- Making uncompressed $IMAGE_NAME ramdisk ------"
 echo "----- Making $IMAGE_NAME ramdisk ------"
 ./release-tools/minigzip < $BIN_DIR/ramdisk-$IMAGE_NAME.cpio > $BIN_DIR/ramdisk-$IMAGE_NAME.img
 echo "----- Making $IMAGE_NAME image ------"
-./release-tools/mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31" --kernel $BIN_DIR/kernel  --ramdisk $BIN_DIR/ramdisk-$IMAGE_NAME.img --base 0x80200000 --ramdiskaddr 0x81500000 --output $BIN_DIR/$IMAGE_NAME.img
+./release-tools/mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31" --kernel $BIN_DIR/kernel  --ramdisk $BIN_DIR/ramdisk-$IMAGE_NAME.img --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000 --output $BIN_DIR/$IMAGE_NAME.img
 
 # create odin image
 cd $BIN_DIR
