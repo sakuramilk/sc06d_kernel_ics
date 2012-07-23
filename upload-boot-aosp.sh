815be97dd57200f68381a2517a4d1f40de584ef7
sakuramilk <c.sakuramilk@gmail.com>
2012-07-23 19:21:19 +0900

    build: change build script name

--- upload-boot-aosp.sh
+++ upload-boot-aosp.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+BINDIR=./out/AOSP/bin
+
 NOSU=$1
 
 if [ "$NOSU" = "nosu" ]; then
-#  adb push ./out/bin/boot.img /tmp/boot.img
-  adb push ./out/bin/boot.img /data/local/tmp/boot.img
+#  adb push $BINDIR/boot.img /tmp/boot.img
+  adb push $BINDIR/boot.img /data/local/tmp/boot.img
 #  adb shell "cat /tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
   adb shell "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
   adb shell "rm /tmp/boot.img"
   adb shell "sync;sync;sync;sleep 2; reboot"
 else
-  adb push ./out/bin/boot.img /data/local/tmp/boot.img
+  adb push $BINDIR/boot.img /data/local/tmp/boot.img
   adb shell su -c "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
   adb shell su -c "rm /data/local/tmp/boot.img"
   adb shell su -c "sync;sync;sync;sleep 2; reboot"
---
+++ upload-boot-samsung.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BINDIR=./out/SAM/bin
+
+NOSU=$1
+
+if [ "$NOSU" = "nosu" ]; then
+#  adb push $BINDIR/boot.img /tmp/boot.img
+  adb push $BINDIR/boot.img /data/local/tmp/boot.img
+#  adb shell "cat /tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
+  adb shell "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
+  adb shell "rm /tmp/boot.img"
+  adb shell "sync;sync;sync;sleep 2; reboot"
+else
+  adb push $BINDIR/boot.img /data/local/tmp/boot.img
+  adb shell su -c "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
+  adb shell su -c "rm /data/local/tmp/boot.img"
+  adb shell su -c "sync;sync;sync;sleep 2; reboot"
+fi
